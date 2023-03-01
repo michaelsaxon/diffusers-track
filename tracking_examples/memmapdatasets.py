fe788aea406dc303ab6c55ea3b90db692edf5e33
Michael Saxon <xmichaelsaxon@gmail.com>
2023-03-01 14:20:19 -0800

    testing my id tracking hack. is hf engineered to let me do this minimal change?

---
+++ tracking_examples/memmapdatasets.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn.functional as F
+
+import datasets
+
+
+# we want image datasets to have keys for image, id, and 
--- tracking_examples/train_track_unconditional.py
+++ tracking_examples/train_track_unconditional.py
@@ -484,7 +484,7 @@ def main(args):
 
     def transform_images(examples):
         images = [augmentations(image.convert("RGB")) for image in examples["image"]]
-        print(examples)
+        
         return {"input": images}
 
     logger.info(f"Dataset size: {len(dataset)}")
