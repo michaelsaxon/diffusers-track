82d1a65669ed7b1ccb56002f23941af36d304da7
Michael Saxon <xmichaelsaxon@gmail.com>
2023-03-01 11:42:20 -0800

    testing my id tracking hack. is hf engineered to let me do this minimal change?

--- examples/unconditional_image_generation/train_track_unconditional.py
+++ examples/unconditional_image_generation/train_track_unconditional.py
@@ -495,7 +495,7 @@ def main(args):
             data, target = cls.__getitem__(self, idx)
             return data, target, idx
 
-        return type(cls.__name__, (cls,), {
+        return type(type(cls), (cls,), {
             '__getitem__': __getitem__,
         })
 
