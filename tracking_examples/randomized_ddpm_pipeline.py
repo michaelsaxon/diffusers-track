bcfcdeff4380708074b0aef848ac01b6b7c98792
Michael Saxon <xmichaelsaxon@gmail.com>
2023-03-13 19:56:31 -0700

    test seed altered

--- tracking_examples/randomized_ddpm_pipeline.py
+++ tracking_examples/randomized_ddpm_pipeline.py
@@ -32,27 +32,9 @@ class DDPMPipeline_seedreset(DiffusionPipeline):
         num_inference_steps: int = 1000,
         output_type: Optional[str] = "pil",
         return_dict: bool = True,
+        reset_generator_step : int = 0,
     ) -> Union[ImagePipelineOutput, Tuple]:
-        r"""
-        Args:
-            batch_size (`int`, *optional*, defaults to 1):
-                The number of images to generate.
-            generator (`torch.Generator`, *optional*):
-                One or a list of [torch generator(s)](https://pytorch.org/docs/stable/generated/torch.Generator.html)
-                to make generation deterministic.
-            num_inference_steps (`int`, *optional*, defaults to 1000):
-                The number of denoising steps. More denoising steps usually lead to a higher quality image at the
-                expense of slower inference.
-            output_type (`str`, *optional*, defaults to `"pil"`):
-                The output format of the generate image. Choose between
-                [PIL](https://pillow.readthedocs.io/en/stable/): `PIL.Image.Image` or `np.array`.
-            return_dict (`bool`, *optional*, defaults to `True`):
-                Whether or not to return a [`~pipelines.ImagePipelineOutput`] instead of a plain tuple.
-
-        Returns:
-            [`~pipelines.ImagePipelineOutput`] or `tuple`: [`~pipelines.utils.ImagePipelineOutput`] if `return_dict` is
-            True, otherwise a `tuple. When returning a tuple, the first element is a list with the generated images.
-        """
+        
         # Sample gaussian noise to begin loop
         if isinstance(self.unet.sample_size, int):
             image_shape = (batch_size, self.unet.in_channels, self.unet.sample_size, self.unet.sample_size)
@@ -70,10 +52,16 @@ class DDPMPipeline_seedreset(DiffusionPipeline):
         self.scheduler.set_timesteps(num_inference_steps)
 
         # reset the seed before we run forward
-        generator = torch.Generator(device=generator.device)
+
+        if reset_generator_step > num_inference_steps:
+            reset_generator_step = num_inference_steps
+
 
         for t in self.progress_bar(self.scheduler.timesteps):
             # 1. predict noise model_output
+            if t == reset_generator_step:
+                generator = torch.Generator(device=generator.device)
+
             model_output = self.unet(image, t).sample
 
             # 2. compute previous image: x_t -> x_t-1
